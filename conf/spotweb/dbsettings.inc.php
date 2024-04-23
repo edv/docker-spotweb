dbfe6c0c95c126d69e4cafa6f5f34e1a0fce5f8a
Erik de Vries <git@erikdevries.nl>
2024-04-23 10:14:09 +0200

    Revert setting db settings to default MySQL when env. variables are not found

--- conf/spotweb/dbsettings.inc.php
+++ conf/spotweb/dbsettings.inc.php
@@ -1,7 +1,7 @@
 <?php
-$dbsettings['engine'] = getenv('DB_ENGINE');
-$dbsettings['host'] = getenv('DB_HOST');
-$dbsettings['port'] = getenv('DB_PORT');
-$dbsettings['dbname'] = getenv('DB_NAME');
-$dbsettings['user'] = getenv('DB_USER');
-$dbsettings['pass'] = getenv('DB_PASS');
+$dbsettings['engine'] = getenv('DB_ENGINE') ?: 'pdo_mysql';
+$dbsettings['host'] = getenv('DB_HOST') ?: 'mysql';
+$dbsettings['port'] = getenv('DB_PORT') ?: '3306';
+$dbsettings['dbname'] = getenv('DB_NAME') ?: 'spotweb';
+$dbsettings['user'] = getenv('DB_USER') ?: 'spotweb';
+$dbsettings['pass'] = getenv('DB_PASS') ?: 'spotweb';
