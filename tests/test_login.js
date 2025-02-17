f1737831f025bfd036035489c54c0f96d423ec96
Erik de Vries <git@erikdevries.nl>
2025-02-17 16:15:33 +0100

    Basic selenium test to see if login is working

---
+++ tests/test_login.js
@@ -0,0 +1,37 @@
+const { Builder, By, until } = require('selenium-webdriver');
+
+(async function testLogin() {
+  const driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get(
+      'http://localhost:8080/?page=login&data[htmlheaderssent]=true'
+    );
+
+    const usernameField = await driver.findElement(
+      By.name('loginform[username]')
+    );
+    await usernameField.sendKeys('admin');
+
+    const passwordField = await driver.findElement(
+      By.name('loginform[password]')
+    );
+    await passwordField.sendKeys('spotweb');
+
+    const submitButton = await driver.findElement(
+      By.name('loginform[submitlogin]')
+    );
+    await submitButton.click();
+
+    // Wait for the specific JSON result
+    const expectedJsonResult =
+      '{"result":"success","data":[],"info":[],"warnings":[],"errors":[]}';
+    await driver.wait(async () => {
+      const result = await driver.executeScript(
+        'return document.body.innerText'
+      );
+      return result.includes(expectedJsonResult);
+    }, 10000);
+  } finally {
+    await driver.quit();
+  }
+})();
